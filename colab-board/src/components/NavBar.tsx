115cb1eb072ff02c476499102387785ffea4fd03
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2023-09-15 18:33:47 -0700

    Learning React
    Working on a scrum board web app with React JS.

---
+++ colab-board/src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import './ComponentStyles.css'
+
+function NavBar(){
+    return (
+        <>
+        <div className='container-fluid'>
+            <div className='navbar'>
+                
+            </div>
+
+
+        </div>
+        </>
+    );
+}
+
+export default NavBar;
